99a373309734b0fc0e1feaa6ae4fe4868bbdb173
Nitesh Virothi <45283322+vvs1999@users.noreply.github.com>
2025-03-23 00:41:27 -0400

    page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Navbar } from "@/components/Navbar";
-import { FaMapMarkerAlt, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaMapMarkerAlt, FaLinkedin, FaTwitter, FaCheck } from "react-icons/fa";
 
 export default function PricingPage() {
   const pricingPlans = [
