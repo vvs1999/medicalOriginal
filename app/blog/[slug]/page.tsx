9f31127c230d1473bfdfb6f9b01a3beb3e135813
Nitesh Virothi <45283322+vvs1999@users.noreply.github.com>
2025-04-03 22:47:39 -0400

    page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { FaArrowLeft, FaMapMarkerAlt, FaLinkedin, FaTwitter, FaCheck, FaUser, Fa
 import Link from "next/link";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-
+import { use } from "react";
 // Define types for blog post data
 type BlogPost = {
   title: string;
